8842057d20ecff71380c86ddd411c5bf711582ed
kovalikovalik <kovalikovalik@yandex.ru>
2022-09-07 11:28:00 +0500

    4.2. Заголовок в файле java [#159036]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -1,7 +1,8 @@
 package ru.job4j;
 
 public class Info {
-    public static void main (String[]args) {
+    @SuppressWarnings("checkstyle:MethodParamPad")
+    public static void main(String[] args) {
         System.out.println("07.09.2022");
     }
 }
