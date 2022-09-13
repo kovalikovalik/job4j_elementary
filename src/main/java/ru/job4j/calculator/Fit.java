c79f09901862546553b2f10e8844df5a1bea7435
kovalikovalik <kovalikovalik@yandex.ru>
2022-09-13 16:25:37 +0500

    Идеальный вес

--- src/main/java/ru/job4j/calculator/Fit.java
+++ src/main/java/ru/job4j/calculator/Fit.java
@@ -5,14 +5,12 @@ public class Fit {
     public static double manWeight(double height) {
         double rsl = (height - 100) * 1.15;
         return rsl;
-
-    }
+            }
 
     public static double womanWeight(double height) {
         double rsl = (height - 110) * 1.15;
         return rsl;
-
-    }
+            }
 
     public static void main(String[] args) {
         double height = 187;
