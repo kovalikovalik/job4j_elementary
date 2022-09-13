d696b570e824d8180d4cad482844d7fb2e4a9cdd
kovalikovalik <kovalikovalik@yandex.ru>
2022-09-13 11:35:19 +0500

    5. Конвертер валюты [#41583 #342480]

--- src/main/java/ru/job4j/Converter.java
+++ src/main/java/ru/job4j/Converter.java
@@ -5,16 +5,16 @@ public class Converter {
         return value / 70;
     }
 
-    public static float rubleToDollar(float value1) {
-        return value1 / 60;
+    public static float rubleToDollar(float value) {
+        return value / 60;
     }
 
-    public static float euroToRuble(float value2) {
-        return value2 * 70;
+    public static float euroToRuble(float value) {
+        return value * 70;
     }
 
-    public static float dollarToRuble(float value3) {
-        return value3 * 60;
+    public static float dollarToRuble(float value) {
+        return value * 60;
     }
 
     public static void main(String[] args) {
