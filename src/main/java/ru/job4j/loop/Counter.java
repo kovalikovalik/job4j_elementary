3b7ace89308053a1bab78193c1cf27656c7a8f7b
kovalikovalik <kovalikovalik@yandex.ru>
2022-09-22 15:57:36 +0500

    5.0.1. Сумма чисел. [#226416 #345541]

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -9,11 +9,12 @@ public class Counter {
         return sum;
     }
 
-    public static void main(String[] args) {
-        System.out.println(sum(0, 10));
-        System.out.println(sum(3, 8));
-        System.out.println(sum(1, 1));
-        System.out.println(sum(6, 9));
-        System.out.println(sum(0, 20));
+        public static void main(String[]args) {
+            System.out.println(sum(0, 10));
+            System.out.println(sum(3, 8));
+            System.out.println(sum(1, 1));
+            System.out.println(sum(6, 9));
+            System.out.println(sum(0, 20));
+        }
     }
-}
+
